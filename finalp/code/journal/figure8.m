cb6564385ea8aa54ddf3f6706fee24e7385df34e
giang <phamhuonggiang.hust@gmail.com>
2022-02-10 10:56:47 +0900

    update plot for figure8

--- finalp/code/journal/figure8.m
+++ finalp/code/journal/figure8.m
@@ -18,22 +18,17 @@ R_b = [0.1 0.2 0.5 1 2]*1e9;
 
 T = 300; 
 
-%------------------
-SI_func = @(sigma_s_2) exp(sigma_s_2) - 1; 
-
 %----------------------------
 
-ber_ana = zeros(length(C_n_2), length(P_t_dBm)); 
-ber_sim = zeros(length(C_n_2), length(P_t_dBm)); 
+ber_ana = zeros(length(R_b), length(C_n_2)); 
+ber_sim = zeros(length(R_b), length(C_n_2)); 
 
 
 
 for isi = 1:length(R_b) 
     for idx = 1:length(C_n_2)
-        si = SI_func(sigma_x_2_func(lambda, C_n_2(idx), L))
-
-        ber_ana(isi, idx) = analysis(si, APD_gain, P_s, T, R_b(isi)); 
-%         ber_sim(isi, idx) = simulation(si, APD_gain, P_s, T, R_b(isi)); 
+        ber_ana(isi, idx) = analysis2(C_n_2(idx), APD_gain, P_s, T, R_b(isi)); 
+        ber_sim(isi, idx) = simulation2(C_n_2(idx), APD_gain, P_s, T, R_b(isi)); 
     end
 end 
 
@@ -43,14 +38,14 @@ sim = {'o', '+', 'v', 's', '^'};
 for isi = 1:length(R_b)
     h1(isi) = semilogy(C_n_2, ber_ana(isi, :), string(ana(isi)));
     hold on 
-%     semilogy(C_n_2, ber_sim(isi, :), string(sim(isi))); 
+    semilogy(C_n_2, ber_sim(isi, :), string(sim(isi))); 
 end 
 legend(h1, 'R_b = 100 Mb/s', 'R_b = 200 Mb/s', ...
     'R_b = 500 Mb/s', ...
     'R_b = 1 Gb/s', ...
     'R_b = 2 Gb/s')
 grid on 
-xlabel('$C^2_n$', 'Interpreter','latex')
+xlabel('C^2_n')
 ylabel('Bit Error Rate')
 ylim([1e-9 1])
 
--- finalp/code/journal/params.m
+++ finalp/code/journal/params.m
@@ -2,6 +2,8 @@ beta_v = 0.1/1e3;
 phi = 1e-3; % rad 
 % d = 0.02; 
 d = 0.044601655266774; 
+lambda = 1.55e-6; 
+L = 1000;
 
 a_func = @(L, D, phi, beta_v) (pi * (D/2)^2)/(pi *(phi * L/2)^2) * exp(-beta_v * L); 
 
